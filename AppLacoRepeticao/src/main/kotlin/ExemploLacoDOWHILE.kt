64487e38f3cfb39dcfaba96a2d2cc53e08e60785
Paulo H. Carvalho <contacthenry96@gmail.com>
2024-07-13 09:08:42 -0300

    Add files via upload

---
+++ AppLacoRepeticao/src/main/kotlin/ExemploLacoDOWHILE.kt
@@ -0,0 +1,34 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+
+    var numeroBinario: Long = reader.nextLong()
+    var numeroDecimal: Int = 0
+
+    numeroDecimal = conversorBinarioDecimal(numeroBinario)
+
+    println("Número binário $numeroBinario = $numeroDecimal")
+
+}
+
+fun conversorBinarioDecimal(numeroBinario:Long): Int {
+
+    var _numeroBinario = numeroBinario
+    var _numeroDecimal = 0
+    var i = 0
+    var numeroTemporario: Long
+
+    while ( _numeroBinario.toInt() != 0 ) {
+
+        numeroTemporario = _numeroBinario % 10
+        _numeroBinario /= 10
+        _numeroDecimal += (numeroTemporario * Math.pow(2.0, i.toDouble())).toInt()
+        i++
+
+    }
+
+    return _numeroDecimal
+
+}
